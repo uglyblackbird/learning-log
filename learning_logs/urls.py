6ed1545169d2685998cf265b0f9756c3ade12dae
n1ckyr0tten <nickyrotten10@gmail.com>
2025-10-01 23:56:49 +0500

    Initial commit

---
+++ learning_logs/urls.py
@@ -0,0 +1,26 @@
+"""Определяет схемы URL для Learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Домашняя страница
+    path('', views.index, name='index'),
+    # Страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Страница для добавления новой темы
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Страница для добавления новой записи
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Страницы для редактирования записи
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    # Страница для удаления записи
+    path('delete_entry/<int:entry_id>/', views.delete_entry, name='delete_entry'),
+    # Страница для удаления темы
+    path('delete_topic/<int:topic_id>/', views.delete_topic, name='delete_topic'),
+    # Страница для редактирования темы
+    path('edit_topic/<int:topic_id>/', views.edit_topic, name='edit_topic'),
+]
